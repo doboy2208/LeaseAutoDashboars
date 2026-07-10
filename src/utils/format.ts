2b8c4d2df82fb8a5e9e04cde22e64d87fe61bcd8
doboy2208 <rio.essed@gmail.com>
2026-07-10 15:19:05 +0200

    Delete src directory

--- src/utils/format.ts
+++
@@ -1,6 +0,0 @@
-export const formatEuro = (value: number) =>
-  new Intl.NumberFormat('nl-NL', {
-    style: 'currency',
-    currency: 'EUR',
-    maximumFractionDigits: value % 1 ? 2 : 0
-  }).format(value);
